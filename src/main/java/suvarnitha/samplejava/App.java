d524221e21fa4c65f05cca594ac40966a5caed07
jessica suvarna <jessicasuvarnitha@gmail.com>
2023-12-11 14:40:28 +0530

    maven java new push

---
+++ src/main/java/suvarnitha/samplejava/App.java
@@ -0,0 +1,13 @@
+package suvarnitha.samplejava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
